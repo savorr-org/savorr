3ee9d4f1e8fbe51192025ef80258f59750fe7d1c
Bryant Le <bryantle2021@gmail.com>
2023-10-22 03:35:07 -0500

    fix: move client init

--- app/api/vision/route.ts
+++ app/api/vision/route.ts
@@ -7,20 +7,20 @@ const credential = JSON.parse(
   Buffer.from(apiKey, "base64").toString()
 );
 
-const client = new vision.ImageAnnotatorClient({
-  projectId: credential.project_id,
-  credentials: {
-    client_email: credential.client_email,
-    private_key: credential.private_key
-  }
-});
-
 const link = "https://media.licdn.com/dms/image/D5603AQGooiAw6TVqBA/profile-displayphoto-shrink_800_800/0/1687936635764?e=1703116800&v=beta&t=J5Av9F1joZbNs3I-u2LHYoKYrKL8GqfE-l5t15-XS8I";
 
 
 // TODO: request for image
 // TODO: return 1 name with highest confidence
 export async function GET() {
+  const client = new vision.ImageAnnotatorClient({
+    projectId: credential.project_id,
+    credentials: {
+      client_email: credential.client_email,
+      private_key: credential.private_key
+    }
+  });
+
   if (!client.objectLocalization) return NextResponse.json({ data: ''});
 
   const [result] = await client.objectLocalization(link);
