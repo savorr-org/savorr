2bdcde9850d6c62af47e99acb081045a8edb159c
rphong <richard.phong424@gmail.com>
2023-10-22 10:37:29 -0500

    feat(saverr): added cheaper ingredients functionality

--- app/shopping-list/page.tsx
+++ app/shopping-list/page.tsx
@@ -1,197 +1,260 @@
-'use client'
-import AutocompleteSearchBar from "@/components/autocompleteSearchBar"
+'use client';
+import AutocompleteSearchBar from '@/components/autocompleteSearchBar';
 import { Table } from '@mantine/core';
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from 'react';
 import { Divider } from '@mantine/core';
 
 interface ShoppingItem {
-    name: string;
-    price: number;
-    quantity: number;
-    subtotal: number;
+  name: string;
+  averagePrice: number;
+  cheapestPrice: number;
+  quantity: number;
+  subtotal: number;
 }
 
 type Product = {
-    name: string;
-  };
+  name: string;
+  averagePrice: number;
+  cheapestPrice: number;
+};
 export default function Page() {
-    const shoppingListElements = [
-        { name: 'Honeycrisp Apple', price: 1.99 },
-        { name: 'Peanut Butter', price: 1.99 },
-        { name: 'Potato Kettle Chips', price: 1.99 }
-    ];
 
-    const handleAddToShoppingList = (product: Product) => {
-        // Create a new item based on the selected product and add it to the shopping list
-        const newItem: ShoppingItem = {
-          name: product.name,
-          price: 0, // Set the price of the product
-          quantity: 1,
-          subtotal: 0, // Calculate the subtotal based on the price and quantity
-        };
-    
-        setShoppingList((prevList) => [...prevList, newItem]);
-      };
-
-    const [shoppingList, setShoppingList] = useState<ShoppingItem[]>(
-        shoppingListElements.map((element) => ({
-        name: element.name,
-        price: element.price,
-        quantity: 1,
-        subtotal: element.price,
-        }))
-    );
-    const [totalTax, setTotalTax] = useState<number>(0);
-    const [totalAmount, setTotalAmount] = useState<number>(0);
-
-    const [modalStates, setModalStates] = useState<boolean[]>(new Array(shoppingList.length).fill(false));
-
-    const handleToggleModal = (index: number) => {
-        const newModalStates = [...modalStates];
-        newModalStates[index] = !newModalStates[index];
-        setModalStates(newModalStates);
+  const handleAddToShoppingList = (product: Product) => {
+    // Create a new item based on the selected product and add it to the shopping list
+    const newItem: ShoppingItem = {
+      name: product.name,
+      averagePrice: product.averagePrice, // Set the price of the product
+      cheapestPrice: product.cheapestPrice, // Set the price of the product
+      quantity: 1,
+      subtotal: product.averagePrice, // Calculate the subtotal based on the price and quantity
     };
 
-    const handleDeleteItem = (index: number) => {
-        const updatedShoppingList = [...shoppingList];
-        updatedShoppingList.splice(index, 1);
-        setShoppingList(updatedShoppingList);
-    };
+    setShoppingList((prevList) => [...prevList, newItem]);
+  };
 
-    const rows = shoppingList.map((item, index) => (
-        <Table.Tr key={item.name}>
-            <Table.Td>{item.name}</Table.Td>
-            <Table.Td>${item.price.toFixed(2)}</Table.Td>
-            <Table.Td>
-                <input
-                    type="number"
-                    className="w-10 flex text-center"
-                    value={item.quantity}
-                    onChange={(e) => handleQuantityChange(e, index)}
-                />
-            </Table.Td>
-            <Table.Td>
-                <div className="flex items-center space-x-4 relative">
-                <span>${item.subtotal.toFixed(2)}</span>
+  const [shoppingList, setShoppingList] = useState<ShoppingItem[]>(
+    []
+  );
+  const [totalTax, setTotalTax] = useState<number>(0);
+  const [cheapestTotal, setCheapestTotal] = useState<number>(0);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
 
-                <button onClick={() => handleToggleModal(index)}>
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 3C9 3.27614 9.22386 3.5 9.5 3.5C9.77614 3.5 10 3.27614 10 3C10 2.72386 9.77614 2.5 9.5 2.5C9.22386 2.5 9 2.72386 9 3Z" fill="black" stroke="black" strokeWidth="0.75" stroke-miterlimit="10"/>
-                    <path d="M9 10C9 10.2761 9.22386 10.5 9.5 10.5C9.77614 10.5 10 10.2761 10 10C10 9.72386 9.77614 9.5 9.5 9.5C9.22386 9.5 9 9.72386 9 10Z" fill="black" stroke="black" strokeWidth="0.75" stroke-miterlimit="10"/>
-                    <path d="M9 17C9 17.2761 9.22386 17.5 9.5 17.5C9.77614 17.5 10 17.2761 10 17C10 16.7239 9.77614 16.5 9.5 16.5C9.22386 16.5 9 16.7239 9 17Z" fill="black" stroke="black" strokeWidth="0.75" stroke-miterlimit="10"/>
-                </svg>
-                </button>
+  const [modalStates, setModalStates] = useState<boolean[]>(
+    new Array(shoppingList.length).fill(false)
+  );
 
-                {modalStates[index] && (
-                    <div className="absolute z-20 bg-white px-6 py-3 border-light-gray border-2 rounded-md flex flex-col right-0 md:right-20 top-7">
-                        <button className="flex justify-center" onClick={() => handleDeleteItem(index)}>
-                            <span className="flex flex-row space-x-3">
-                            <span className="font-manrope">Delete Item</span>
-                            <svg width="20" height="20" viewBox="0 0 37 41" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M30.793 8.14919L29.1514 32.7723C29.0077 34.929 28.9358 36.0073 28.47 36.8249C28.0599 37.5448 27.4413 38.1235 26.6958 38.4848C25.849 38.8952 24.7683 38.8952 22.6068 38.8952H14.3824C12.2209 38.8952 11.1402 38.8952 10.2934 38.4848C9.54786 38.1235 8.9293 37.5448 8.51921 36.8249C8.05341 36.0073 7.98152 34.929 7.83774 32.7723L6.1962 8.14919M2.09674 8.14919H34.8924M26.6935 8.14919L26.1388 6.48496C25.6012 4.87219 25.3324 4.0658 24.8338 3.46962C24.3936 2.94315 23.8282 2.53564 23.1895 2.28445C22.4662 2 21.6162 2 19.9162 2H17.073C15.3729 2 14.5229 2 13.7997 2.28445C13.161 2.53564 12.5956 2.94315 12.1554 3.46962C11.6568 4.0658 11.388 4.87219 10.8504 6.48496L10.2957 8.14919M22.594 16.3481V30.6962M14.3951 16.3481V30.6962" stroke="#69C34A" strokeWidth="2.45968" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg>
-                            </span>
-                        </button>
-                    </div>
-                )}
+  const handleToggleModal = (index: number) => {
+    const newModalStates = [...modalStates];
+    newModalStates[index] = !newModalStates[index];
+    setModalStates(newModalStates);
+  };
 
-                </div>
-            </Table.Td>
-        </Table.Tr>
-    ));
-    const recipesList = [
-        { name: 'Applenut Krunch' },
-        { name: 'Chicken Alfredo' },
-    ];
+  const handleDeleteItem = (index: number) => {
+    const updatedShoppingList = [...shoppingList];
+    updatedShoppingList.splice(index, 1);
+    setShoppingList(updatedShoppingList);
+  };
 
-    const recipes = recipesList.map((item, index) => (
-        <>
-        <div className="flex flex-row font-manrope font-bold justify-between">
-            <div className="flex flex-row justify-center items-center space-x-3">
-            <span>{item.name}</span>
-            <svg width="30" height="30" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="41" height="41" rx="8" fill="#69C34A"/>
-                <path d="M13.5 11.9C9.91015 11.9 7 14.8102 7 18.4C7 21.0654 8.60433 23.3561 10.9 24.3592V27.825H29.1V24.3592C31.3957 23.3561 33 21.0654 33 18.4C33 14.8102 30.0898 11.9 26.5 11.9C26.1743 11.9 25.8543 11.9239 25.5414 11.9702C24.7585 9.66148 22.5732 8 20 8C17.4268 8 15.2415 9.66148 14.4586 11.9702C14.1457 11.9239 13.8257 11.9 13.5 11.9Z" fill="white"/>
-                <path d="M29.0978 29.775H10.9022C10.9154 31.591 11.0085 32.5855 11.6615 33.2385C12.423 34 13.6487 34 16.1 34H23.9C26.3513 34 27.5769 34 28.3385 33.2385C28.9916 32.5855 29.0845 31.591 29.0978 29.775Z" fill="white"/>
+  const rows = shoppingList.map((item, index) => (
+    <Table.Tr key={item.name}>
+      <Table.Td>{item.name}</Table.Td>
+      <Table.Td>${item.averagePrice}</Table.Td>
+      <Table.Td>
+        <input
+          type='number'
+          className='w-10 flex text-center'
+          value={item.quantity}
+          onChange={(e) => handleQuantityChange(e, index)}
+        />
+      </Table.Td>
+      <Table.Td>
+        <div className='flex items-center space-x-4 relative'>
+          <span>${item.subtotal}</span>
+
+          <button onClick={() => handleToggleModal(index)}>
+            <svg
+              width='20'
+              height='20'
+              viewBox='0 0 20 20'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M9 3C9 3.27614 9.22386 3.5 9.5 3.5C9.77614 3.5 10 3.27614 10 3C10 2.72386 9.77614 2.5 9.5 2.5C9.22386 2.5 9 2.72386 9 3Z'
+                fill='black'
+                stroke='black'
+                strokeWidth='0.75'
+                stroke-miterlimit='10'
+              />
+              <path
+                d='M9 10C9 10.2761 9.22386 10.5 9.5 10.5C9.77614 10.5 10 10.2761 10 10C10 9.72386 9.77614 9.5 9.5 9.5C9.22386 9.5 9 9.72386 9 10Z'
+                fill='black'
+                stroke='black'
+                strokeWidth='0.75'
+                stroke-miterlimit='10'
+              />
+              <path
+                d='M9 17C9 17.2761 9.22386 17.5 9.5 17.5C9.77614 17.5 10 17.2761 10 17C10 16.7239 9.77614 16.5 9.5 16.5C9.22386 16.5 9 16.7239 9 17Z'
+                fill='black'
+                stroke='black'
+                strokeWidth='0.75'
+                stroke-miterlimit='10'
+              />
             </svg>
-            </div>
+          </button>
 
-            <button className="bg-green w-36 text-white mx-8 py-2 rounded-md text-lg">
-                Add to list
-            </button>
+          {modalStates[index] && (
+            <div className='absolute z-20 bg-white px-6 py-3 border-light-gray border-2 rounded-md flex flex-col right-0 md:right-20 top-7'>
+              <button
+                className='flex justify-center'
+                onClick={() => handleDeleteItem(index)}
+              >
+                <span className='flex flex-row space-x-3'>
+                  <span className='font-manrope'>Delete Item</span>
+                  <svg
+                    width='20'
+                    height='20'
+                    viewBox='0 0 37 41'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M30.793 8.14919L29.1514 32.7723C29.0077 34.929 28.9358 36.0073 28.47 36.8249C28.0599 37.5448 27.4413 38.1235 26.6958 38.4848C25.849 38.8952 24.7683 38.8952 22.6068 38.8952H14.3824C12.2209 38.8952 11.1402 38.8952 10.2934 38.4848C9.54786 38.1235 8.9293 37.5448 8.51921 36.8249C8.05341 36.0073 7.98152 34.929 7.83774 32.7723L6.1962 8.14919M2.09674 8.14919H34.8924M26.6935 8.14919L26.1388 6.48496C25.6012 4.87219 25.3324 4.0658 24.8338 3.46962C24.3936 2.94315 23.8282 2.53564 23.1895 2.28445C22.4662 2 21.6162 2 19.9162 2H17.073C15.3729 2 14.5229 2 13.7997 2.28445C13.161 2.53564 12.5956 2.94315 12.1554 3.46962C11.6568 4.0658 11.388 4.87219 10.8504 6.48496L10.2957 8.14919M22.594 16.3481V30.6962M14.3951 16.3481V30.6962'
+                      stroke='#69C34A'
+                      strokeWidth='2.45968'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    />
+                  </svg>
+                </span>
+              </button>
+            </div>
+          )}
         </div>
-        <br/>
-        </>
-    ));
+      </Table.Td>
+    </Table.Tr>
+  ));
+  const recipesList = [
+    { name: 'Applenut Krunch' },
+    { name: 'Chicken Alfredo' },
+  ];
 
-    const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-        const newQuantity = parseInt(e.target.value);
-        if (isNaN(newQuantity) || newQuantity < 1) return;
-
-        const updatedShoppingList = [...shoppingList];
-        updatedShoppingList[index].quantity = newQuantity;
-        updatedShoppingList[index].subtotal = newQuantity * updatedShoppingList[index].price;
-
-        setShoppingList(updatedShoppingList);
-    };
-
-    useEffect(() => {
-        let total = shoppingList.reduce((acc, item) => acc + item.subtotal, 0);
-        let tax = total * 0.0725
-        total += totalTax
-        setTotalAmount(total);
-        setTotalTax(tax);
-    }, [shoppingList, totalTax]);
-
-    return (
-        <div className="bg-white container mx-auto px-4 md:px-12 md:pt-2">            
-            <AutocompleteSearchBar onAddToShoppingList={handleAddToShoppingList} />
-
-            <div className="text-3xl text-green font-manrope font-bold pt-10 pb-10">
-                Shopping List
-            </div>
-
-            <Table>
-                <Table.Thead className="bg-light-green">
-                    <Table.Tr>
-                        <Table.Th>Item(s)</Table.Th>
-                        <Table.Th>Price</Table.Th>
-                        <Table.Th>Quantity</Table.Th>
-                        <Table.Th>Subtotal</Table.Th>
-                    </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>{rows}</Table.Tbody>
-            </Table>
-
-            <Divider my="sm" />
-
-            <div className="flex justify-end">
-                <div className="flex flex-col text-right">
-                    <div className="space-x-20">
-                        <span>Tax (7.25%)</span>
-                        <span>${totalTax.toFixed(2)}</span>
-                    </div>
-
-                    <div className="space-x-20">
-                        <span>Total Amount</span>
-                        <span>${totalAmount.toFixed(2)}</span>
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="text-3xl text-green font-manrope font-bold pt-10 pb-10">
-                Recipes
-            </div>
-
-            <div className="flex flex-col">
-            {recipes}
-            </div>
-
-            <button className="bg-green text-white py-2 rounded-md text-lg w-full mt-10 mb-20 font-manrope font-bold">
-                Start Savor
-            </button>
-
+  const recipes = recipesList.map((item, index) => (
+    <>
+      <div className='flex flex-row font-manrope font-bold justify-between'>
+        <div className='flex flex-row justify-center items-center space-x-3'>
+          <span>{item.name}</span>
+          <svg
+            width='30'
+            height='30'
+            viewBox='0 0 41 41'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <rect width='41' height='41' rx='8' fill='#69C34A' />
+            <path
+              d='M13.5 11.9C9.91015 11.9 7 14.8102 7 18.4C7 21.0654 8.60433 23.3561 10.9 24.3592V27.825H29.1V24.3592C31.3957 23.3561 33 21.0654 33 18.4C33 14.8102 30.0898 11.9 26.5 11.9C26.1743 11.9 25.8543 11.9239 25.5414 11.9702C24.7585 9.66148 22.5732 8 20 8C17.4268 8 15.2415 9.66148 14.4586 11.9702C14.1457 11.9239 13.8257 11.9 13.5 11.9Z'
+              fill='white'
+            />
+            <path
+              d='M29.0978 29.775H10.9022C10.9154 31.591 11.0085 32.5855 11.6615 33.2385C12.423 34 13.6487 34 16.1 34H23.9C26.3513 34 27.5769 34 28.3385 33.2385C28.9916 32.5855 29.0845 31.591 29.0978 29.775Z'
+              fill='white'
+            />
+          </svg>
         </div>
-    );
+
+        <button className='bg-green w-36 text-white mx-8 py-2 rounded-md text-lg'>
+          Add to list
+        </button>
+      </div>
+      <br />
+    </>
+  ));
+
+  const handleQuantityChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const newQuantity = parseInt(e.target.value);
+    if (isNaN(newQuantity) || newQuantity < 1) return;
+
+    const updatedShoppingList = [...shoppingList];
+    updatedShoppingList[index].quantity = newQuantity;
+    updatedShoppingList[index].subtotal =
+      newQuantity * updatedShoppingList[index].averagePrice;
+
+    setShoppingList(updatedShoppingList);
+  };
+
+  useEffect(() => {
+    let total = 0;
+    let cheapestTotal = 0;
+    shoppingList.forEach(element => {
+      console.log(element);
+      total += element.subtotal;
+      cheapestTotal += element.cheapestPrice;
+    });
+    let tax = total * 0.0725;
+    total += totalTax;
+    setTotalAmount(total);
+    setCheapestTotal(cheapestTotal);
+    setTotalTax(tax);
+  }, [shoppingList, totalTax]);
+
+  const [buttonClick, setButtonClick] = useState(false)
+
+  return (
+    <div className='bg-white container mx-auto px-4 md:px-12 md:pt-2'>
+      <AutocompleteSearchBar onAddToShoppingList={handleAddToShoppingList} />
+
+      <div className='text-3xl text-green font-manrope font-bold pt-10 pb-10'>
+        Shopping List
+      </div>
+
+      <Table>
+        <Table.Thead className='bg-light-green'>
+          <Table.Tr>
+            <Table.Th>Item(s)</Table.Th>
+            <Table.Th>Price</Table.Th>
+            <Table.Th>Quantity</Table.Th>
+            <Table.Th>Subtotal</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+
+      <Divider my='sm' />
+
+      <div className='flex justify-end'>
+        <div className='flex flex-col text-right'>
+          <div className='space-x-20'>
+            <span>Tax (7.25%)</span>
+            <span>${totalTax.toFixed(2)}</span>
+          </div>
+
+          <div className='space-x-20'>
+            <span>Total Amount</span>
+            <span>${totalAmount.toFixed(2)}</span>
+            { buttonClick && (
+              <>
+              <span className="text-green">${cheapestTotal.toFixed(2)}</span>
+              <div className='text-green'>You saved {(totalAmount - cheapestTotal).toFixed(2)}!</div>
+              </>
+            )
+            }
+          </div>
+        </div>
+      </div>
+
+      <div className='text-3xl text-green font-manrope font-bold pt-10 pb-10'>
+        Recipes
+      </div>
+
+      <div className='flex flex-col'>{recipes}</div>
+
+      <button onClick={() => setButtonClick(!buttonClick)} className='bg-green text-white py-2 rounded-md text-lg w-full mt-10 mb-20 font-manrope font-bold'>
+        Start Savor
+      </button>
+    </div>
+  );
 }
--- components/autocompleteSearchBar.tsx
+++ components/autocompleteSearchBar.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { useState, useEffect, useRef } from "react";
-import ProductList from "./productList";
-import SearchInput from "./searchInput";
+import { useState, useEffect, useRef } from 'react';
+import ProductList from './productList';
+import SearchInput from './searchInput';
 
 interface ShoppingItem {
   name: string;
@@ -11,20 +11,26 @@ interface ShoppingItem {
 }
 
 interface AutocompleteSearchBarProps {
-  name: string, 
-  price: string
+  name: string;
+  averagePrice: number;
+  cheapestPrice: number;
+  cheapestName: string;
 }
 
 type Product = {
   name: string;
+  averagePrice: number;
+  cheapestPrice: number;
 };
 
 type Props = {
-  onAddToShoppingList: React.Dispatch<React.SetStateAction<AutocompleteSearchBarProps>>
-}
+  onAddToShoppingList: React.Dispatch<
+    React.SetStateAction<AutocompleteSearchBarProps>
+  >;
+};
 
-export default function AutocompleteSearchBar ({onAddToShoppingList}: Props) {
-  const [query, setQuery] = useState("");
+export default function AutocompleteSearchBar({ onAddToShoppingList }: Props) {
+  const [query, setQuery] = useState('');
   const [products, setProducts] = useState<Product[]>([]);
   const [selectedProductIndex, setSelectedProductIndex] = useState<number>(-1);
   const [searchResults, setSearchResults] = useState<Product[]>([]);
@@ -32,18 +38,18 @@ export default function AutocompleteSearchBar ({onAddToShoppingList}: Props) {
 
   useEffect(() => {
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({query})
+      body: JSON.stringify({ query }),
     };
 
     fetch('/api/search', options)
       .then((res) => res.json())
       .then((data) => {
         setSearchResults(data.data);
-      })
+      });
 
     if (!query) {
       setSearchResults([]);
@@ -61,36 +67,54 @@ export default function AutocompleteSearchBar ({onAddToShoppingList}: Props) {
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "ArrowUp") {
+    if (event.key === 'ArrowUp') {
       setSelectedProductIndex((prevIndex) =>
         prevIndex === -1 ? searchResults.length - 1 : prevIndex - 1
       );
-    } else if (event.key === "ArrowDown") {
+    } else if (event.key === 'ArrowDown') {
       setSelectedProductIndex((prevIndex) =>
         prevIndex === searchResults.length - 1 ? -1 : prevIndex + 1
       );
-    } else if (event.key === "Enter") {
+    } else if (event.key === 'Enter') {
       if (selectedProductIndex !== -1) {
         const selectedProduct = searchResults[selectedProductIndex];
         alert(`You selected ${selectedProduct.name}`);
-        setQuery("");
+        setQuery('');
         setSelectedProductIndex(-1);
         setSearchResults([]);
       }
     }
   };
 
-  const handleProductClick = () => {
+  const handleProductClick = async () => {
     //alert(`You selected ${product.name}`);
-    alert(`You selected ${query}`)
+    alert(`You selected ${query}`);
     if (query) {
+      let name = query;
+      let average = 1;
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          term: name,
+          locationId: localStorage.getItem('location'),
+          limit: 50,
+        }),
+      };
+
+      const response = await fetch('/api/kroger/price', options);
+      const data = await response.json();
+      average = data.data.average;
+      const cheapest = data.data.lowest.price;
+      const cheapestItem = data.data.lowest.item;
       onAddToShoppingList({
-        name: query, 
-        price: "1.99" // TODO: add price of item here
+        name: name,
+        averagePrice: average ? parseFloat(average.toFixed(2)) : 3.45,
+        cheapestPrice: cheapest != Number.MAX_SAFE_INTEGER ?  parseFloat(cheapest.toFixed(2)) : 1.36,
+        cheapestName: cheapestItem
       });
     }
 
-    setQuery("");
+    setQuery('');
     setSelectedProductIndex(-1);
   };
 
@@ -98,9 +122,9 @@ export default function AutocompleteSearchBar ({onAddToShoppingList}: Props) {
     const activeProduct = document.getElementById(`product-${index}`);
     if (activeProduct) {
       activeProduct.scrollIntoView({
-        block: "nearest",
-        inline: "start",
-        behavior: "smooth",
+        block: 'nearest',
+        inline: 'start',
+        behavior: 'smooth',
       });
     }
   };
@@ -112,36 +136,49 @@ export default function AutocompleteSearchBar ({onAddToShoppingList}: Props) {
   }, [selectedProductIndex]);
 
   return (
-    <div className="flex flex-col mt-10 mx-auto relative items-center">
-      <div className="flex flex-row space-x-2 w-full">
+    <div className='flex flex-col mt-10 mx-auto relative items-center'>
+      <div className='flex flex-row space-x-2 w-full'>
         <SearchInput
           value={query}
           onChange={handleQueryChange}
           onKeyDown={handleKeyDown}
           inputRef={inputRef}
-          placeholder="Enter product..."
+          placeholder='Enter product...'
         />
-  
+
         <button
-          className="bg-green w-36 text-white mx-10 py-3 rounded-md text-lg"
+          className='bg-green w-36 text-white mx-10 py-3 rounded-md text-lg'
           onClick={() => handleProductClick()}
         >
           + Add
         </button>
 
-        <button
-          className="md:hidden bg-light-gray w-36 mx-10 py-3 rounded-md text-lg flex justify-center items-center"
-          
-        >
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10C10.3431 10 9 11.3431 9 13C9 14.6569 10.3431 16 12 16Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-          <path d="M3 16.8V9.2C3 8.0799 3 7.51984 3.21799 7.09202C3.40973 6.71569 3.71569 6.40973 4.09202 6.21799C4.51984 6 5.0799 6 6.2 6H7.25464C7.37758 6 7.43905 6 7.49576 5.9935C7.79166 5.95961 8.05705 5.79559 8.21969 5.54609C8.25086 5.49827 8.27836 5.44328 8.33333 5.33333C8.44329 5.11342 8.49827 5.00346 8.56062 4.90782C8.8859 4.40882 9.41668 4.08078 10.0085 4.01299C10.1219 4 10.2448 4 10.4907 4H13.5093C13.7552 4 13.8781 4 13.9915 4.01299C14.5833 4.08078 15.1141 4.40882 15.4394 4.90782C15.5017 5.00345 15.5567 5.11345 15.6667 5.33333C15.7216 5.44329 15.7491 5.49827 15.7803 5.54609C15.943 5.79559 16.2083 5.95961 16.5042 5.9935C16.561 6 16.6224 6 16.7454 6H17.8C18.9201 6 19.4802 6 19.908 6.21799C20.2843 6.40973 20.5903 6.71569 20.782 7.09202C21 7.51984 21 8.0799 21 9.2V16.8C21 17.9201 21 18.4802 20.782 18.908C20.5903 19.2843 20.2843 19.5903 19.908 19.782C19.4802 20 18.9201 20 17.8 20H6.2C5.0799 20 4.51984 20 4.09202 19.782C3.71569 19.5903 3.40973 19.2843 3.21799 18.908C3 18.4802 3 17.9201 3 16.8Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <button className='md:hidden bg-light-gray w-36 mx-10 py-3 rounded-md text-lg flex justify-center items-center'>
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10C10.3431 10 9 11.3431 9 13C9 14.6569 10.3431 16 12 16Z'
+              stroke='black'
+              strokeWidth='1.5'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+            <path
+              d='M3 16.8V9.2C3 8.0799 3 7.51984 3.21799 7.09202C3.40973 6.71569 3.71569 6.40973 4.09202 6.21799C4.51984 6 5.0799 6 6.2 6H7.25464C7.37758 6 7.43905 6 7.49576 5.9935C7.79166 5.95961 8.05705 5.79559 8.21969 5.54609C8.25086 5.49827 8.27836 5.44328 8.33333 5.33333C8.44329 5.11342 8.49827 5.00346 8.56062 4.90782C8.8859 4.40882 9.41668 4.08078 10.0085 4.01299C10.1219 4 10.2448 4 10.4907 4H13.5093C13.7552 4 13.8781 4 13.9915 4.01299C14.5833 4.08078 15.1141 4.40882 15.4394 4.90782C15.5017 5.00345 15.5567 5.11345 15.6667 5.33333C15.7216 5.44329 15.7491 5.49827 15.7803 5.54609C15.943 5.79559 16.2083 5.95961 16.5042 5.9935C16.561 6 16.6224 6 16.7454 6H17.8C18.9201 6 19.4802 6 19.908 6.21799C20.2843 6.40973 20.5903 6.71569 20.782 7.09202C21 7.51984 21 8.0799 21 9.2V16.8C21 17.9201 21 18.4802 20.782 18.908C20.5903 19.2843 20.2843 19.5903 19.908 19.782C19.4802 20 18.9201 20 17.8 20H6.2C5.0799 20 4.51984 20 4.09202 19.782C3.71569 19.5903 3.40973 19.2843 3.21799 18.908C3 18.4802 3 17.9201 3 16.8Z'
+              stroke='black'
+              strokeWidth='1.5'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
           </svg>
-
         </button>
-
       </div>
-      {query !== "" && searchResults.length > 0 && (
+      {query !== '' && searchResults.length > 0 && (
         <ProductList
           products={searchResults}
           selectedProductIndex={selectedProductIndex}
@@ -150,4 +187,4 @@ export default function AutocompleteSearchBar ({onAddToShoppingList}: Props) {
       )}
     </div>
   );
-};
+}
